219d9496b8170188e782e38d6014f85a5bec796a
katemcg93 <katemcg93@gmail.com>
2021-11-22 10:59:32 +0000

    seed summary of purpose

--- seed.py
+++ seed.py
@@ -1,5 +1,10 @@
 import numpy as np
 from numpy import random
+import matplotlib.pyplot as plt
+import seaborn as sns
 np.random.seed(42)
-random_numbers = np.random.randint(1, 10, size = 5)
+random_numbers = np.random.randint(1, 100, size = 5)
 print(random_numbers)
+
+sns.histplot(random_numbers)
+plt.show()
