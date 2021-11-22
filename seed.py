aea24dde0c6df6a32ec63efc686aa0cf11ac415e
katemcg93 <katemcg93@gmail.com>
2021-11-22 20:06:41 +0000

    references and conclusion

--- seed.py
+++ seed.py
@@ -8,14 +8,3 @@ print(random_numbers)
 
 sns.histplot(random_numbers)
 plt.show()
-
-rng1 = np.random.default_rng(100)
-rng2 = np.random.default_rng(45)
-
-def random_array (x):
-    random_numbers = x.integers(1,100,size = 5)
-    return random_numbers
-
-random_numbers1 = random_array(x = rng1)
-random_number2 = random_array(x = rng2)
-print(random_numbers1, "\n", random_number2)
